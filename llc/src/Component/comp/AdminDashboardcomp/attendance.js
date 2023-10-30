01818c29255bef11cac3028b257333b1be1f8e0c
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-10-30 09:22:02 +0200

    done with modal and toast

--- llc/src/Component/comp/AdminDashboardcomp/attendance.js
+++ llc/src/Component/comp/AdminDashboardcomp/attendance.js
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function TeacherAttendance() {
+const TeacherAttendance = () => {
   const [attendanceData, setAttendanceData] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch('http://localhost:8000/user/getTeacherAtendance');
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        const data = await response.json();
-        setAttendanceData(data.data);
+        const response = await axios.get('http://localhost:8000/user/getTeacherAtendance');
+        setAttendanceData(response.data.data);
         setLoading(false);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -23,22 +20,25 @@ function TeacherAttendance() {
     fetchData();
   }, []);
 
-  const handleAttendanceChange = async (user_id, attend) => {
-    const endpoint = attend === 1
-      ? `http://localhost:8000/user/updateTeacherAttendance/${user_id}`
-      : `http://localhost:8000/user/updateTeacherAttendanceneg/${user_id}`;
+  const handleAttendanceChange = async (id, newValue) => {
+    const endpoint = newValue === '1'
+      ? `http://localhost:8000/user/updateActivationforuser/${id}`
+      : `http://localhost:8000/user/updateActivationforuserneg/${id}`;
 
     try {
-      const response = await fetch(endpoint, {
-        method: 'PUT',
+      await axios.put(endpoint, null, {
         headers: {
           'Content-Type': 'application/json',
         },
       });
-      if (!response.ok) {
-        throw new Error('Failed to update attendance. Status: ' + response.status);
-      }
-      // You can also update the state or perform other actions here as needed.
+
+      const updatedData = attendanceData.map(item => {
+        if (item.user_id === id) {
+          return { ...item, attend: newValue };
+        }
+        return item;
+      });
+      setAttendanceData(updatedData);
     } catch (error) {
       console.error('Error updating attendance:', error);
     }
@@ -67,20 +67,24 @@ function TeacherAttendance() {
               <td>{item.email}</td>
               <td>{item.role}</td>
               <td>
-                <input
-                  type="radio"
-                  name={`attendance_${index}`}
-                  value="1"
-                  checked={item.attend === 1}
-                  onChange={() => handleAttendanceChange(item.user_id, 1)}
-                /> Yes
-                <input
-                  type="radio"
-                  name={`attendance_${index}`}
-                  value="0"
-                  checked={item.attend === 0}
-                  onChange={() => handleAttendanceChange(item.user_id, 0)}
-                /> No
+                <label>
+                  <input
+                    type="radio"
+                    name={`attendance_${index}`}
+                    value="1"
+                    checked={item.attend === '1'}
+                    onChange={() => handleAttendanceChange(item.user_id, '1')}
+                  /> Yes
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    name={`attendance_${index}`}
+                    value="0"
+                    checked={item.attend === '0'}
+                    onChange={() => handleAttendanceChange(item.user_id, '0')}
+                  /> No
+                </label>
               </td>
               <td>{item.level}</td>
               <td>{item.title}</td>
