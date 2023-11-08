0946d3ce0a0b58e616fd712a76fb7df9714ac2f9
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-11-08 19:41:53 +0200

    .

--- llc/src/Component/comp/attendance.js
+++ llc/src/Component/comp/attendance.js
@@ -1,15 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../styles/attendance.css'
 
 const TeacherAttendance = () => {
   const [attendanceData, setAttendanceData] = useState([]);
   const [loading, setLoading] = useState(true);
-
+ 
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/user/getTeacherAtendance`);
+        const response = await axios.get('http://localhost:8000/user/getTeacherAtendance');
         setAttendanceData(response.data.data);
         setLoading(false);
       } catch (error) {
@@ -23,8 +22,8 @@ const TeacherAttendance = () => {
 
   const handleAttendanceChange = async (id, newValue) => {
     const endpoint = newValue === '1'
-      ? `${process.env.REACT_APP_API_URL}/user/updateTeacherAttendance/${id}`
-      : `${process.env.REACT_APP_API_URL}/user/updateTeacherAttendanceneg/${id}`;
+      ? `http://localhost:8000/user/updateTeacherAttendance/${id}`
+      : `http://localhost:8000/user/updateTeacherAttendanceneg/${id}`;
 
     try {
       await axios.put(endpoint, {
@@ -51,44 +50,43 @@ const TeacherAttendance = () => {
   }
 
   return (
-    <div className="teacher-attendance-container">
-      <h1 className="teacher-attendance-title">Teacher Attendance</h1>
-      <table className="teacher-attendance-table">
+    <div>
+      <h1>Teacher Attendance</h1>
+      <table>
         <thead>
           <tr>
-            <th className="email-header">Email</th>
-            <th className="attendance-header">Attendance</th>
-            <th className="level-header">Level</th>
-            <th className="title-header">Title</th>
+            <th>Email</th>
+            <th>Attendance</th>
+            <th>Level</th>
+            <th>Title</th>
           </tr>
         </thead>
         <tbody>
           {attendanceData.map((item, index) => (
             <tr key={index}>
-              <td className="email-data">{item.email}</td>
-
-              <td className="attendance-data">
-                <label className="attendance-label">
+              <td>{item.email}</td>
+              <td>
+                <label>
                   <input
                     type="radio"
                     name={`attendance_${index}`}
                     value="1"
-                    checked={item.attend === '1'}
+                    checked={item.attend === 1}
                     onChange={() => handleAttendanceChange(item.user_id, '1')}
                   /> Yes
                 </label>
-                <label className="attendance-label">
+                <label>
                   <input
                     type="radio"
                     name={`attendance_${index}`}
                     value="0"
-                    checked={item.attend === '0'}
+                    checked={item.attend === 0}
                     onChange={() => handleAttendanceChange(item.user_id, '0')}
                   /> No
                 </label>
               </td>
-              <td className="level-data">{item.level}</td>
-              <td className="title-data">{item.title}</td>
+              <td>{item.level}</td>
+              <td>{item.title}</td>
             </tr>
           ))}
         </tbody>
@@ -96,6 +94,5 @@ const TeacherAttendance = () => {
     </div>
   );
 }
-
 
 export default TeacherAttendance;
