7b467e4fd6a31caf95eed69d08145ac1a2545ee4
batoulll-ghosn <batoul.ghosn@isae.edu.lb>
2023-11-08 15:43:21 +0200

    i hope its the before last commit

--- llc/src/Component/comp/login.js
+++ llc/src/Component/comp/login.js
@@ -15,7 +15,7 @@ const Login = () => {
   const handleLogin = async () => {
     const token = captchaRef.current.getValue();
     try {
-        const response = await fetch("http://localhost:8000/user/getAll", {
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/user/getAll`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
